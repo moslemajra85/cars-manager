93c46dceb23ec99cf26147e6e89fd19565a10560
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2025-03-20 02:45:23 +0100

    comment the code

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { carsReducer, changeSearchTerm, addCar, removeCar } from './carsSlice';
-import { formReducer, changeName, changeCost } from './formSlices';
+import { configureStore } from '@reduxjs/toolkit'; // Import the configureStore function from Redux Toolkit
+import { carsReducer, changeSearchTerm, addCar, removeCar } from './carsSlice'; // Import the cars reducer and actions
+import { formReducer, changeName, changeCost } from './formSlices'; // Import the form reducer and actions
 
+// Configure the Redux store with the reducers
 const store = configureStore({
   reducer: {
-    form: formReducer,
-    cars: carsReducer,
+    form: formReducer, // Add the form reducer to the store
+    cars: carsReducer, // Add the cars reducer to the store
   },
 });
 
+// Export the store and actions for use in the application
 export { store, changeName, changeCost, changeSearchTerm, addCar, removeCar };
